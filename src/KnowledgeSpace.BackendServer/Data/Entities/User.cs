46bd3b51efa0a8db70faebce21c5b4a39f08c825
DaiLT <letandai1304@gmail.com>
2025-05-26 14:25:51 +0700

    Create entity class

---
+++ src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnowledgeSpace.BackendServer.Data.Entities
+{
+    [Table("Users")]
+    public class User : IdentityUser
+    {
+        [MaxLength(50)]
+        [Required]
+        public string FirstName { get; set; } = null!; 
+
+        [MaxLength(50)]
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public DateTime Dob { get; set; }
+        public int? NumberOfKnowledgeBases { get; set; }
+        public int? NumberOfVotes { get; set; }
+        public int? NumberOfReports { get; set; }
+    }
+}
---
+++ src/KnowledgeSpace.BackendServer/Data/Entities/Vote.cs
@@ -0,0 +1,17 @@
+﻿using KnowledgeSpace.BackendServer.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnowledgeSpace.BackendServer.Data.Entities
+{
+    [Table("Votes")]
+    public class Vote : IDateTracking
+    {
+        public int KnowledgeBaseId { get; set; }
+        [MaxLength(50)]
+        [Column(TypeName = "varchar(50)")]
+        public string UserId { get; set; } = null!;
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
